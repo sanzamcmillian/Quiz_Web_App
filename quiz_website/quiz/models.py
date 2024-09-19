0756e9518a9e3c1f8e35719f5bfe8359056b1c9f
Xolisakesi <xolisakesi@gmail.com>
2024-09-19 09:43:42 +0200

    final models

--- quiz_website/quiz/models.py
+++ quiz_website/quiz/models.py
@@ -1,37 +1,20 @@
 # models.py
 from django.db import models
+from django.conf import settings  # Import to reference the custom user model
+from users.models import CustomUser
 
-class Types(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-class Quiz(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class QuizResult(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  
+    quiz_score = models.FloatField() 
 
     def __str__(self):
-        return self.title
+        return f"{self.user.username} - {self.quiz_score}"
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=255)
-    question_type = models.ForeignKey(Types, on_delete=models.CASCADE, related_name='questions')
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.question_text
-
-class Answer(models.Model):
-    answer_text = models.CharField(max_length=255)
-    is_correct = models.BooleanField(default=False)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    created_at = models.DateTimeField(auto_now_add=True)
+class Leaderboard(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  
+    total_score = models.FloatField(default=0)
+    rank = models.IntegerField(null=True, blank=True) 
 
     def __str__(self):
-        return self.answer_text
+        return f"{self.user.username} - Rank: {self.rank}, Score: {self.total_score}"
