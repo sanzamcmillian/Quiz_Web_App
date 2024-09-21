f575bff63a8a645d8719a4533c635c0f810bf41b
sanzamcmillian <g18s2658@ru.ac.za>
2024-09-21 22:38:06 +0200

    models update
    clean up code

--- quiz_website/quiz/models.py
+++ quiz_website/quiz/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User  # Import the built-in User model
-
 from django.db import models
 
+
 class QuizResult(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  
-    quiz_score = models.FloatField() 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz_score = models.FloatField()
 
     def __str__(self):
         return f"{self.user.username} - {self.quiz_score}"
 
 
 class Leaderboard(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_score = models.FloatField(default=0)
-    rank = models.IntegerField(null=True, blank=True) 
+    rank = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return f'{self.total_score}'
+        return f"{self.user.username} - Rank: {self.rank}, Score: {self.total_score}"
