28f23652c40ea141c20eb1ee19af9c0619ba4453
sanzamcmillian <g18s2658@ru.ac.za>
2024-09-21 22:30:46 +0200

    commenting and cleaning code

--- quiz_website/quiz/views.py
+++ quiz_website/quiz/views.py
@@ -14,16 +14,13 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 
 
-def logout_view(request):
-    """Handle user logout."""
-    request.user.auth_token.delete()  # Delete the token
-    logout(request)
-    return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
-
+def landing_view(request):
+    """method to render home page"""
+    return render(request, 'index.html')
 
 
 def home_view(request):
-    # List of quiz categories or topics
+    """List of quiz categories or topics"""
     categories = [
         "General Knowledge", "Science",
         "History", "Entertainment", "Mythology",
@@ -32,7 +29,24 @@ def home_view(request):
     ]
     return render(request, 'quiz_detail.html', {'categories': categories})
 
+
+def register_view(request):
+    """auth to handle user registration"""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log the user in automatically after registration
+            login(request, user)
+            return redirect('quiz_detail')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
 def login_view(request):
+    """auth to handle login for user"""
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -48,27 +62,12 @@ def login_view(request):
     return render(request, 'login.html', {'form': form})
 
 
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            # Log the user in automatically after registration
-            login(request, user)
-            return redirect('quiz_detail')
-    else:
-        form = UserCreationForm()
+def logout_view(request):
+    """Handle user logout."""
+    request.user.auth_token.delete()
+    logout(request)
+    return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
 
-    return render(request, 'register.html', {'form': form})
-
-
-def landing_view(request):
-    return render(request, 'index.html')
-
-
-def quiz_detail_view(request, category):
-    # Display details about the quiz and a start button
-    return render(request, 'quiz_detail.html', {'category': category})
 
 def quiz_view(request, category):
     """Fetch quiz questions from an external API and handle form submission"""
@@ -89,8 +88,9 @@ def quiz_view(request, category):
     }
     return render(request, 'quiz_view.html', context)
 
+
 def result_view(request):
-    # Retrieve the score from query parameters
+    """Retrieve the score from query parameters"""
     score = request.GET.get('score', None)
     if score is not None:
         try:
