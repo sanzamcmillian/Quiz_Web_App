d8f20af7037a8a2e2ca173782c946abb353010aa
sanzamcmillian <g18s2658@ru.ac.za>
2024-09-21 22:42:30 +0200

    final push

--- quiz_website/quiz/forms.py
+++ quiz_website/quiz/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)  # Add an email field if needed
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
 
 class QuizForm(forms.Form):
     def __init__(self, *args, **kwargs):
